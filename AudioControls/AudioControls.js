0e982d3b5a46e89abf31165518fb83746e853683
faketon <chalernponman@hotmail.com>
2018-04-02 17:44:14 +0700

    Update AudioControls.js

--- AudioControls/AudioControls.js
+++ AudioControls/AudioControls.js
@@ -86,8 +86,8 @@ class AudioControls extends Component {
     super(props);
 
     this.state = {
-      isVideo:false,
-      typeScene:0,
+      isVideo: false,
+      typeScene: 0,
       modalVisible: false,
       duration: 0,
       currentTime: 0,
@@ -102,7 +102,7 @@ class AudioControls extends Component {
     AudioController.init(playlist, initialTrack, this.onChangeStatus, this.updateCurrentTime);
   }
 
-  onStatusHeader(){
+  onStatusHeader() {
     return 'hidetop'
   }
 
@@ -134,7 +134,6 @@ class AudioControls extends Component {
   updateCurrentTime = (seconds) => {
     this.setState({currentTime: seconds});
   }
-
 
   renderPlayerIconResize() {
     const {isPlaying} = this.state;
@@ -194,16 +193,16 @@ class AudioControls extends Component {
   renderNextIcon() {
     if (AudioController.hasNext()) {
       return (<TouchableOpacity onPress={() => AudioController.playNext()}>
-          <Image source={require('../../../src/images/icon_next_at.png')} style={styles.controlExtraButton}/>
+        <Image source={require('../../../src/images/icon_next_at.png')} style={styles.controlExtraButton}/>
       </TouchableOpacity>);
     }
-    return (  <Image source={require('../../../src/images/icon_next_at.png')} style={styles.controlExtraButton}/>);
+    return (<Image source={require('../../../src/images/icon_next_at.png')} style={styles.controlExtraButton}/>);
   }
 
   renderPreviousIcon() {
     if (AudioController.hasPrevious()) {
       return (<TouchableOpacity onPress={() => AudioController.playPrevious()}>
-          <Image source={require('../../../src/images/icon_previous_at.png')} style={styles.controlExtraButton}/>
+        <Image source={require('../../../src/images/icon_previous_at.png')} style={styles.controlExtraButton}/>
       </TouchableOpacity>);
     }
     return (<Image source={require('../../../src/images/icon_previous_at.png')} style={styles.controlExtraButton}/>);
@@ -249,26 +248,38 @@ class AudioControls extends Component {
     </TouchableOpacity>)
   }
 
-  renderVideo(){
-    return(<View style={styles.videoContainer}>
-      <Video source={{uri: 'http://www.podcastgenerator.net/demoV2/pg/media/2016-08-13_video3dprinters.mp4'}}   // Can be a URL or a local file.
-           ref={(ref) => {
-             this.player = ref
-           }}                                      // Store reference
-           rate={1.0}                              // 0 is paused, 1 is normal.
-           volume={1.0}                            // 0 is muted, 1 is normal.
-           muted={false}                           // Mutes the audio entirely.
-           paused={false}                          // Pauses playback entirely.
-           resizeMode="cover"                      // Fill the whole screen at aspect ratio.*
-           repeat={true}                           // Repeat forever.
-           playInBackground={false}                // Audio continues to play when app entering background.
-           playWhenInactive={false}                // [iOS] Video continues to play when control or notification center are shown.
-           ignoreSilentSwitch={"ignore"}           // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
-           progressUpdateInterval={250.0}          // [iOS] Interval to fire onProgress (default to ~250ms)
-           style={styles.videoStyle}/>
-    </View>
-
-    )
+  renderVideo() {
+    return (<View style={styles.videoContainer}>
+      <Video source={{
+          uri: 'http://www.podcastgenerator.net/demoV2/pg/media/2016-08-13_video3dprinters.mp4'
+        }}
+        // Can be a URL or a local file.
+        ref={(ref) => {
+          this.player = ref
+        }}
+        // Store reference
+        rate={1.0}
+        // 0 is paused, 1 is normal.
+        volume={1.0}
+        // 0 is muted, 1 is normal.
+        muted={false}
+        // Mutes the audio entirely.
+        paused={false}
+        // Pauses playback entirely.
+        resizeMode="cover"
+        // Fill the whole screen at aspect ratio.*
+        repeat={true}
+        // Repeat forever.
+        playInBackground={false}
+        // Audio continues to play when app entering background.
+        playWhenInactive={false}
+        // [iOS] Video continues to play when control or notification center are shown.
+        ignoreSilentSwitch={"ignore"}
+        // [iOS] ignore | obey - When 'ignore', audio will still play with the iOS hard silent switch set to silent. When 'obey', audio will toggle with the switch. When not specified, will inherit audio settings as usual.
+        progressUpdateInterval={250.0}
+        // [iOS] Interval to fire onProgress (default to ~250ms)
+        style={styles.videoStyle}/>
+    </View>)
   }
 
   renderTrack() {
@@ -276,12 +287,7 @@ class AudioControls extends Component {
     return (<View>
       <View style={styles.center}>
         <View style={styles.playbackContainer}>
-          <Slider value={currentTime} maximumValue={duration}
-            style={styles.playbackBar}
-            minimumTrackTintColor={this.props.sliderMinimumTrackTintColor || this.props.activeColor}
-             maximumTrackTintColor={this.props.sliderMaximumTrackTintColor || this.props.inactiveColor}
-             thumbTintColor={this.props.sliderThumbTintColor || this.props.activeColor}
-             onSlidingComplete={seconds => {
+          <Slider value={currentTime} maximumValue={duration} style={styles.playbackBar} minimumTrackTintColor={this.props.sliderMinimumTrackTintColor || this.props.activeColor} maximumTrackTintColor={this.props.sliderMaximumTrackTintColor || this.props.inactiveColor} thumbTintColor={this.props.sliderThumbTintColor || this.props.activeColor} onSlidingComplete={seconds => {
               AudioController.seek(seconds);
               if (seconds < duration)
                 AudioController.play();
@@ -302,55 +308,58 @@ class AudioControls extends Component {
 
   renderRadioMini() {
     const {currentTime, duration, currentAudio} = this.state;
-    return ( <View style={[styles.containerResize]}>
-        <View style={styles.detailLeft}>
+    return (<View style={styles.playerResize}>
+      <View style={styles.detailLeft}>
+        <TouchableOpacity activeOpacity={0.8} hitSlop={{
+            top: 20,
+            bottom: 20,
+            left: 50,
+            right: 50
+          }} onPress={() => {
+            Actions.refresh({
+              action: {
+                from: '/player',
+                to: '/player/',
+                type: 'hideHeader',
+                refresh: Math.random()
+              }
+            })
+            this.setState({typeScene: 1})
+          }}>
+          <Image source={require('../../../src/images/icon_arrow_up.png')}/>
+        </TouchableOpacity>
+        <Image source={currentAudio.thumbnailUri
+            ? {
+              uri: currentAudio.thumbnailUri
+            }
+            : currentAudio.thumbnailLocal} style={{
+            width: 48,
+            height: 48,
+            borderRadius: 48 / 2
+          }}/>
+      </View>
+      <View style={styles.detailCenter}>
+        <Text style={styles.titleStyleResize}>{currentAudio.title}</Text>
+        <View style={styles.detailCenterSub}>
+          <Text numberOfLines={1} style={styles.contentStyle}>14.00-17.00 / Thu-Fri</Text>
           <TouchableOpacity activeOpacity={0.8} hitSlop={{
               top: 20,
               bottom: 20,
               left: 50,
               right: 50
-            }}
-            onPress={() => {
-              Actions.refresh({action:{
-                    from:'/player',
-                    to:'/player/',
-                    type:'hideHeader',
-                    refresh:Math.random()
-                }})
-              this.setState({typeScene:1})
-            }}>
-                <Image source={require('../../../src/images/icon_arrow_up.png')}/>
+            }} style={styles.buttonLiveResize}>
+            <Image source={require('../../../src/images/icon_live.png')}/>
           </TouchableOpacity>
-          <Image source={currentAudio.thumbnailUri
-              ? {
-                uri: currentAudio.thumbnailUri
-              }
-              : currentAudio.thumbnailLocal} style={{width: 48, height: 48, borderRadius: 48/2}}/>
-        </View>
-        <View style={styles.detailCenter}>
-          <Text style={styles.titleStyleResize}>{currentAudio.title}</Text>
-            <View style={styles.detailCenterSub}>
-              <Text numberOfLines={1} style={styles.contentStyle}>14.00-17.00 / Thu-Fri</Text>
-              <TouchableOpacity activeOpacity={0.8} hitSlop={{
-                  top: 20,
-                  bottom: 20,
-                  left: 50,
-                  right: 50
-                }} style={styles.buttonLiveResize}>
-                <Image source={require('../../../src/images/icon_live.png')}/>
-              </TouchableOpacity>
-            </View>
-        </View>
-        <View style={styles.center}>
-          {this.renderSkipbackwardIcon()}
-          {/* {this.renderPreviousIcon()} */}
-          {this.renderPlayerIconResize()}
-          {/* {this.renderNextIcon()} */}
-          {this.renderSkipforwardIcon()}
         </View>
       </View>
-
-    )
+      <View style={styles.center}>
+        {this.renderSkipbackwardIcon()}
+        {/* {this.renderPreviousIcon()} */}
+        {this.renderPlayerIconResize()}
+        {/* {this.renderNextIcon()} */}
+        {this.renderSkipforwardIcon()}
+      </View>
+    </View>)
   }
 
   renderRadio() {
@@ -369,19 +378,21 @@ class AudioControls extends Component {
           }}>
               <Image source={require('../../../src/images/icon_arrow_down.png')}/>
         </TouchableOpacity>
-
-      </View> */}
+      </View> */
+      }
 
       <Header style={styles.header}>
         <Left style={styles.flex}>
           <Button style={styles.transparent} onPress={() => {
-            Actions.refresh({action:{
-                  from:'/player',
-                  to:'/player/',
-                  type:'showHeader',
-                  refresh:Math.random()
-              }})
-              this.setState({typeScene:0})
+              Actions.refresh({
+                action: {
+                  from: '/player',
+                  to: '/player/',
+                  type: 'showHeader',
+                  refresh: Math.random()
+                }
+              })
+              this.setState({typeScene: 0})
             }}>
             <Image source={require('../../../src/images/icon_arrow_down.png')}/>
           </Button>
@@ -389,8 +400,7 @@ class AudioControls extends Component {
         <Body style={styles.navigationTitle}>
           <Title style={styles.title}></Title>
         </Body>
-        <Right style={styles.flex}>
-        </Right>
+        <Right style={styles.flex}></Right>
       </Header>
 
       <View style={styles.container}>
@@ -404,26 +414,25 @@ class AudioControls extends Component {
         </TouchableOpacity>
 
         {
-          this.state.isVideo == false ?
-          <View style={this.props.thumbnailSize}>
-            <Image source={currentAudio.thumbnailUri
-                ? {
-                  uri: currentAudio.thumbnailUri
-                }
-                : currentAudio.thumbnailLocal} style={this.props.thumbnailSize}/>
-          </View>
-          :
-          this.state.isVideo == true ?
-          <View style={styles.videoContent}>
-            {this.renderVideo()}
-          </View>
-          :
-          <View/>
+          this.state.isVideo == false
+            ? <View style={this.props.thumbnailSize}>
+                <Image source={currentAudio.thumbnailUri
+                    ? {
+                      uri: currentAudio.thumbnailUri
+                    }
+                    : currentAudio.thumbnailLocal} style={this.props.thumbnailSize}/>
+              </View>
+            : this.state.isVideo == true
+              ? <View style={styles.videoContent}>
+                  {this.renderVideo()}
+                </View>
+              : <View/>
 
         }
         {/* <View style={styles.videoContent}>
           {this.renderVideo()}
-        </View> */}
+        </View> */
+        }
         <View style={styles.detailContainer}>
           <Text style={this.props.titleStyle}>{currentAudio.title}</Text>
           {/* <Text style={this.props.authorStyle}>{currentAudio.author}</Text> */}
@@ -441,23 +450,19 @@ class AudioControls extends Component {
                         {currentTime ? moment(currentTime * 1000).format('mm:ss') : '00:00'}
                   </Text> */
           }
-          <Slider value={currentTime} maximumValue={duration}
-            style={styles.playbackBar}
-            minimumTrackTintColor={this.props.sliderMinimumTrackTintColor || this.props.activeColor}
-             maximumTrackTintColor={this.props.sliderMaximumTrackTintColor || this.props.inactiveColor}
-             thumbTintColor={this.props.sliderThumbTintColor || this.props.activeColor}
-             onSlidingComplete={seconds => {
+          <Slider value={currentTime} maximumValue={duration} style={styles.playbackBar} minimumTrackTintColor={this.props.sliderMinimumTrackTintColor || this.props.activeColor} maximumTrackTintColor={this.props.sliderMaximumTrackTintColor || this.props.inactiveColor} thumbTintColor={this.props.sliderThumbTintColor || this.props.activeColor} onSlidingComplete={seconds => {
               AudioController.seek(seconds);
               if (seconds < duration)
                 AudioController.play();
-              }} onValueChange={() => AudioController.clearCurrentTimeListener()}/>
-              {/* <Text numberOfLines={1} style={this.props.sliderTimeStyle}>
+              }} onValueChange={() => AudioController.clearCurrentTimeListener()}/> {/* <Text numberOfLines={1} style={this.props.sliderTimeStyle}>
                         {duration ? moment(duration * 1000).format('mm:ss') : '00:00'}
                   </Text> */
-              }
+          }
         </View>
         <View style={styles.buttonGroup}>
-          <TouchableOpacity activeOpacity={0.8} onPress={()=> this.setState({isVideo:!this.state.isVideo})}>
+          <TouchableOpacity activeOpacity={0.8} onPress={() => this.setState({
+              isVideo: !this.state.isVideo
+            })}>
             <Image source={require('../../../src/images/icon_video.png')}/>
           </TouchableOpacity>
           <TouchableOpacity activeOpacity={0.8}><Image source={require('../../../src/images/icon_speech_bubble.png')}/></TouchableOpacity>
@@ -474,20 +479,22 @@ class AudioControls extends Component {
       {/* {this.renderRadio()} */}
       {/* {this.renderTrack()} */}
       {
-        this.state.typeScene == 1 ?
-        <Animatable.View style={{flex:1}} animation="fadeInUpBig">
-          {/* <View style={{flex:1}}> */}
-            {this.renderRadio()}
-          {/* </View> */}
-        </Animatable.View>
+        this.state.typeScene == 1
+          ? <Animatable.View style={{
+                flex: 1
+              }} animation="fadeInUpBig">
+              {/* <View style={{flex:1}}> */}
+              {this.renderRadio()}
+              {/* </View> */}
+            </Animatable.View>
 
-        :
-        this.state.typeScene == 0 ?
-        <Animatable.View style={{flex:1}} animation="fadeInUp">
-          {this.renderRadioMini()}
-        </Animatable.View>
-        :
-        <View/>
+          : this.state.typeScene == 0
+            ? <Animatable.View style={{
+                  flex: 1
+                }} animation="fadeInUp">
+                {this.renderRadioMini()}
+              </Animatable.View>
+            : <View/>
       }
 
     </View>);
@@ -496,9 +503,7 @@ class AudioControls extends Component {
 
 const mapStateToProps = state => {
 
-  return {
-
-  };
+  return {};
 };
 
 export default connect(mapStateToProps, null)(AudioControls);
